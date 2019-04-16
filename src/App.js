ffcfcdd8745606dbd058277c690b69cf4a65b48b
rckrieger <rebecca.c.krieger@gmail.com>
2019-04-16 10:40:02 -0700

    changed tabs to a sticky nav bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import Page from './Page'
 import './App.css';
 import { Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootswatch/dist/journal/bootstrap.css";
 
 var Terms = {
   title: "Terms",
@@ -15,12 +16,34 @@ var Privacy = {
   title: "Privacy Policy",
 };
 var Landing = {
-  title: "Artizain - Handmade",
+  title: "Home",
 };
-var Handmade = {
-  title: "Amazon",
+var Feature = {
+  title: "Features",
 };
-const TABS = [Landing, AboutUS,Terms, Privacy, Handmade];
+const TABS = [Landing, Feature, Terms, Privacy, AboutUS];
+
+/*{        <Nav
+          variant="tabs"
+          defaultActiveKey={activePlace}
+          onSelect={index => {
+            this.setState({ activePlace: index });
+          }}
+        >
+          {TABS.map((page, index) => (
+            <Nav.Item>
+              <Nav.Link key={index} eventKey={index}>{page.title}</Nav.Link>
+            </Nav.Item>
+          ))}}</Nav>*/
+
+/*
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+                  aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            Here
+          </button>
+ */
+
 
 class App extends Component {
   constructor()
@@ -34,20 +57,31 @@ class App extends Component {
     const activePlace = this.state.activePlace;
     return (
       <div className="App">
-        <Nav
-          variant="tabs"
-          defaultActiveKey={activePlace}
-          onSelect={index => {
-            this.setState({ activePlace: index });
-          }}
-        >
-          {TABS.map((page, index) => (
-            <Nav.Item>
-              <Nav.Link key={index} eventKey={index}>{page.title}</Nav.Link>
-            </Nav.Item>
-          ))}
+       <Navbar>
+        <Nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light"  defaultActiveKey={activePlace}
+             onSelect={index => {
+               this.setState({ activePlace: index });
+             }}>
+          <a className="navbar-brand" href="#">Artizan</a>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav mr-auto">
+                {TABS.map((page, index) => (
+                  <li>
+                    <Nav.Link key={index} eventKey={index}>{page.title}</Nav.Link>
+                  </li>
+                ))}
+              <li>
+                <a className="nav-link" href="https://services.amazon.com/handmade/handmade.html">Handmade</a>
+              </li>
+
+            </ul>
+          </div>
+          <button className="download" type="button">
+            Download
+          </button>
         </Nav>
-        <Page page={TABS[activePlace]}/>
+       </Navbar>
+         <Page page={TABS[activePlace]}/>
       </div>
     );
   }
