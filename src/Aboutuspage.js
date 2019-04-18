6f1aa3d0124c351910fe2ef231d889c920724871
rckrieger <rebecca.c.krieger@gmail.com>
2019-04-18 01:37:15 -0700

    added timeline

--- src/Aboutuspage.js
+++ src/Aboutuspage.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/spacelab/bootstrap.css";
 import { Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
+import { Timeline, TimelineEvent } from '@mailtop/horizontal-timeline'
+import { GoRocket } from 'react-icons/go'
+import { GoPencil } from 'react-icons/go'
+import { GoTools } from 'react-icons/go'
 
 class AboutUsPage extends Component {
   constructor(props) {
@@ -15,6 +19,27 @@ class AboutUsPage extends Component {
           <Row>
             <div className="Title">
               <h1>About Us</h1>
+            </div>
+          </Row>
+          <Row>
+            <div>
+              <Timeline minEvents={3} placeholder>
+                <TimelineEvent
+                  icon={GoPencil}
+                  title='Fall 2018'
+                  subtitle='design'
+                />
+                <TimelineEvent
+                  icon={GoTools}
+                  title='Winter 2019'
+                  subtitle='Build'
+                />
+                <TimelineEvent
+                  icon={GoRocket}
+                  title='Fall 2018'
+                  subtitle='Deploy'
+                />
+              </Timeline>
             </div>
           </Row>
         </Container>
