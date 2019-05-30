c44912b486c3907d6bde3cfde74aef2f18d1aa79
rckrieger <rebecca.c.krieger@gmail.com>
2019-05-30 10:20:57 -0700

    about us done

--- src/Aboutuspage.js
+++ src/Aboutuspage.js
@@ -6,6 +6,11 @@ import { Timeline, TimelineEvent } from '@mailtop/horizontal-timeline'
 import { GoRocket } from 'react-icons/go'
 import { GoPencil } from 'react-icons/go'
 import { GoTools } from 'react-icons/go'
+import ReactContactForm from 'react-mail-form';
+import styled from 'styled-components';
+import largecorg from "./fullkeyboard.gif";
+import globe from "./globe.jpg"
+import orange from "./orange.gif";
 
 class AboutUsPage extends Component {
   constructor(props) {
@@ -17,6 +22,13 @@ class AboutUsPage extends Component {
   }
 
   render() {
+    const Form = styled(ReactContactForm)`
+margin:auto;max-width:800px;min-height:100vh;font-size:14px;text-align:center;
+input,textarea{display:block;margin:12px auto;width:100%;max-width:720px;border:1px solid #555;outline:0;font-size:16px;}
+input{padding:12px 6px;}
+textarea{padding:6px;}
+a{display:block;margin:auto;width:480px;height:3em;line-height:3em;color:#fff;background-color:#3B9CFF;font-size:16px;font-weight:900;text-decoration:blink;&:visited,&:hover,&:focus,&:active{color:#fff;}&:hover{opacity:.7;}}
+`;
     return (
       <div className="TermsPage">
         <Container>
@@ -27,31 +39,59 @@ class AboutUsPage extends Component {
           </Row>
           <Row>
             <div className="Mission">
+              <br/>
               <h2>Our mission is to connect artisans in developing countries with Amazon's existing consumer base of millions
                 in order to grow the artisan economy and provide Amazon customers a greater selection.
               </h2>
+              <br/>
             </div>
           </Row>
           <Row>
-            <div>
+            <img
+              className="d-block w-100"
+              src={orange}
+              alt="Third slide"
+            />
+            <br/>
+          </Row>
+          <Row>
+            <h3><br/>Get to Know Us</h3>
+            <div align="left">
+            <h5><br/>This project was created by Cal Poly, Slo software engineering
+              students as a project for their capstone class as requested by Amazon. This project will likely not be
+            monitored after June 2019 given that the students involved are graduating. Learn more at: </h5>
+              <a href="http://users.csc.calpoly.edu/~djanzen/setopics/capstone/" target="_blank">http://users.csc.calpoly.edu/~djanzen/setopics/capstone/</a>
+            <br/>
+            <br/>
+            <br/>
+
+            </div>
+          </Row>
+          <Row>
+            <div align="center">
               <Timeline minEvents={3} placeholder>
                 <TimelineEvent
                   icon={GoPencil}
                   title='Fall 2018'
-                  subtitle='design'
+                  subtitle='Design'
                 />
                 <TimelineEvent
                   icon={GoTools}
+                  color='#87a2c7'
                   title='Winter 2019'
                   subtitle='Build'
                 />
                 <TimelineEvent
+                  color='#64a338'
                   icon={GoRocket}
                   title='Fall 2018'
                   subtitle='Deploy'
                 />
               </Timeline>
             </div>
+          </Row>
+          <Row>
+            <Form to=	"rileycam@amazon.com"/>
           </Row>
         </Container>
       </div>
--- src/Privacypage.js
+++ src/Privacypage.js
@@ -3,7 +3,10 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/spacelab/bootstrap.css";
 import { Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
 import balance from "./rufusgif.gif";
-
+import corg from "./corgi_keypad.gif";
+import largecorg from "./fullkeyboard.gif";
+import rufus from "./rufus_balance.gif";
+import box from "./dogbox.gif"
 class PrivacyPage extends Component {
   constructor(props) {
     super(props);
@@ -40,46 +43,64 @@ class PrivacyPage extends Component {
             </div>
           </Row>
           <Row>
-            <Col md={5} sm={5}>
               <div align="left">
                 <br/><h2>What types of data do we collect</h2>
-                <p>We collect from NGO organizers:</p>
-                <ul>
-                  <li>Your name</li>
-                  <li>Your email address</li>
-                  <li>Your phone number</li>
-                  <li>Your location when the app is open</li>
-                  <li>The amount of time you have the app open</li>
-                  <li>The ways you interact with the app</li>
-                  <li>Your organization's name</li>
-                  <li>Your organization's address</li>
-                  <li>Your organization's summary</li>
-                </ul>
-                <p>We collect from Artisans:</p>
-                <ul>
-                  <li>Their name</li>
-                  <li>Their email address</li>
-                  <li>Their phone number</li>
-                  <li>Their picture</li>
-                  <li>Pictures and videos of their products</li>
-                  <li>Descriptions of their products</li>
-                  <li>A profile picture of the artisan</li>
-                  <li>A biography of the artisan</li>
-                  <li>Data relating to the artisan's sales</li>
-                </ul>
+                <br/>
+                <Container>
+                  <Row>
+                    <Col md={4} sm={4}>
+                      <h5>We collect from NGO organizers:</h5>
+                      <ul>
+                        <li>Your name</li>
+                        <li>Your email address</li>
+                        <li>Your phone number</li>
+                        <li>Your location when the app is open</li>
+                        <li>The amount of time you have the app open</li>
+                        <li>The ways you interact with the app</li>
+                        <li>Your organization's name</li>
+                        <li>Your organization's address</li>
+                        <li>Your organization's summary</li>
+                      </ul>
+                    </Col>
+                    <Col md={4} sm={4}>
+                      <h5>We collect from Artisans:</h5>
+                      <ul>
+                        <li>Their name</li>
+                        <li>Their email address</li>
+                        <li>Their phone number</li>
+                        <li>Their picture</li>
+                        <li>Pictures and videos of their products</li>
+                        <li>Descriptions of their products</li>
+                        <li>A profile picture of the artisan</li>
+                        <li>A biography of the artisan</li>
+                        <li>Data relating to the artisan's sales</li>
+                      </ul>
+                    </Col>
+                    <Col md={4} sm={4}>
+                      <img
+                        className="inner"
+                        src={box}
+                        alt="Third slide"
+                      />
+                    </Col>
+                  </Row>
+                </Container>
               </div>
+          </Row>
+          <Row>
             <div align="left">
               <h2>How do we use your information</h2>
-              <br/><p>We use your information to prov</p>
+              <br/><p>We use your information to provide customized serives to better fit your organization's needs.</p>
             </div>
-            </Col>
-            <Col md={7} sm={7}>
-            </Col>
           </Row>
           <Row>
             <Col md={7} sm={7}>
               <div>
-                {RUFUS}
+                <img
+                  className="inner"
+                  src={largecorg}
+                  alt="Third slide"
+                />
               </div>
             </Col>
             <Col md={5} sm = {5}>
